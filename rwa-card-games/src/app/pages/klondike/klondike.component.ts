004d093ce1f94b465a4d26bc987caa73f92ff271
Milan Stojanovski <milanstojanovski1@gmail.com>
2024-08-05 14:23:50 +0200

    ui updated (improved card scaling)

--- rwa-card-games/src/app/pages/klondike/klondike.component.ts
+++ rwa-card-games/src/app/pages/klondike/klondike.component.ts
@@ -9,21 +9,21 @@ import { Router } from '@angular/router';
 })
 export class KlondikeComponent {
   
-  foundationClubs: Card[];
-  foundationDiamonds: Card[];
-  foundationHearts: Card[];
-  foundationSpades: Card[];
+  foundationClubs: Card[] = [];
+  foundationDiamonds: Card[] = [];
+  foundationHearts: Card[] = [];
+  foundationSpades: Card[] = [];
 
-  deckStock: Card[];
-  deckWaste: Card[];
+  deckStock: Card[] = [];
+  deckWaste: Card[] = [];
 
-  tableau1: Card[];
-  tableau2: Card[];
-  tableau3: Card[];
-  tableau4: Card[];
-  tableau5: Card[];
-  tableau6: Card[];
-  tableau7: Card[];
+  tableau1: Card[] = [];
+  tableau2: Card[] = [];
+  tableau3: Card[] = [];
+  tableau4: Card[] = [];
+  tableau5: Card[] = [];
+  tableau6: Card[] = [];
+  tableau7: Card[] = [];
 
   tableau1_movable: number = 1;
   tableau2_movable: number = 1;
@@ -33,30 +33,130 @@ export class KlondikeComponent {
   tableau6_movable: number = 1;
   tableau7_movable: number = 1;
 
-  draggedCard: Card | null;
-  draggedStartLocation: Card[] | null;
+  draggedCard: Card | null = null;
+  draggedStartLocation: Card[] | null = null;
 
-  constructor(private router: Router) {
-    this.foundationClubs = [];
-    this.foundationDiamonds = [];
-    this.foundationHearts = [];
-    this.foundationSpades = [];
+  constructor() {
+    this.startNewGameDemo();
+  }
 
+  startNewGameDemo() {
     this.deckStock = [];
     this.deckWaste = [];
 
-    this.tableau1 = [];
-    this.tableau2 = [];
-    this.tableau3 = [];
-    this.tableau4 = [];
-    this.tableau5 = [];
-    this.tableau6 = [];
-    this.tableau7 = [];
+    this.fillDeckDemo();
+    this.fisherYatesShuffleDemo(this.deckStock);
+    this.PlaceTableauCardsDemo();
+  }
 
-    this.draggedCard = null;
-    this.draggedStartLocation = null;
+  fillDeckDemo(): void {
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Ace));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Two));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Three));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Four));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Five));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Six));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Seven));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Eight));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Nine));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Ten));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Jack));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.Queen));
+    this.deckStock.push(new Card(CardSuit.CLUBS, CardNumber.King));
 
-    this.loadCardsDemo();
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Ace));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Two));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Three));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Four));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Five));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Six));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Seven));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Eight));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Nine));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Ten));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Jack));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.Queen));
+    this.deckStock.push(new Card(CardSuit.DIAMONDS, CardNumber.King));
+
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Ace));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Two));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Three));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Four));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Five));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Six));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Seven));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Eight));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Nine));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Ten));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Jack));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.Queen));
+    this.deckStock.push(new Card(CardSuit.HEARTS, CardNumber.King));
+
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Ace));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Two));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Three));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Four));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Five));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Six));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Seven));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Eight));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Nine));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Ten));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Jack));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.Queen));
+    this.deckStock.push(new Card(CardSuit.SPADES, CardNumber.King));
+  }
+
+  fisherYatesShuffleDemo(deck: Card[]): void {
+    for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+  }
+
+  PlaceTableauCardsDemo(): void {
+    this.tableau1.push(this.deckStock.pop()!);
+    
+    this.tableau2.push(this.deckStock.pop()!);
+    this.tableau2.push(this.deckStock.pop()!);
+
+    this.tableau3.push(this.deckStock.pop()!);
+    this.tableau3.push(this.deckStock.pop()!);
+    this.tableau3.push(this.deckStock.pop()!);
+    
+    this.tableau4.push(this.deckStock.pop()!);
+    this.tableau4.push(this.deckStock.pop()!);
+    this.tableau4.push(this.deckStock.pop()!);
+    this.tableau4.push(this.deckStock.pop()!);
+
+    this.tableau5.push(this.deckStock.pop()!);
+    this.tableau5.push(this.deckStock.pop()!);
+    this.tableau5.push(this.deckStock.pop()!);
+    this.tableau5.push(this.deckStock.pop()!);
+    this.tableau5.push(this.deckStock.pop()!);
+
+    this.tableau6.push(this.deckStock.pop()!);
+    this.tableau6.push(this.deckStock.pop()!);
+    this.tableau6.push(this.deckStock.pop()!);
+    this.tableau6.push(this.deckStock.pop()!);
+    this.tableau6.push(this.deckStock.pop()!);
+    this.tableau6.push(this.deckStock.pop()!);
+
+    this.tableau7.push(this.deckStock.pop()!);
+    this.tableau7.push(this.deckStock.pop()!);
+    this.tableau7.push(this.deckStock.pop()!);
+    this.tableau7.push(this.deckStock.pop()!);
+    this.tableau7.push(this.deckStock.pop()!);
+    this.tableau7.push(this.deckStock.pop()!);
+    this.tableau7.push(this.deckStock.pop()!);
+
+    this.deckWaste.push(this.deckStock.pop()!);
+    this.deckWaste.push(this.deckStock.pop()!);
+    this.deckWaste.push(this.deckStock.pop()!);
+    this.deckWaste.push(this.deckStock.pop()!);
+  }
+
+  draw3(): void {
     
   }
 
@@ -79,35 +179,4 @@ export class KlondikeComponent {
       }
     }
   }
-
-  loadCardsDemo() {
-    this.tableau2.push(new Card(CardSuit.SPADES, CardNumber.Ace));
-
-    this.tableau3.push(new Card(CardSuit.SPADES, CardNumber.Two));
-    this.tableau3.push(new Card(CardSuit.DIAMONDS, CardNumber.Two));
-    
-    this.tableau4.push(new Card(CardSuit.CLUBS, CardNumber.Three));
-    this.tableau4.push(new Card(CardSuit.HEARTS, CardNumber.Three));
-    this.tableau4.push(new Card(CardSuit.HEARTS, CardNumber.Three));
-
-    this.tableau5.push(new Card(CardSuit.SPADES, CardNumber.Four));
-    this.tableau5.push(new Card(CardSuit.CLUBS, CardNumber.Four));
-    this.tableau5.push(new Card(CardSuit.HEARTS, CardNumber.Four));
-    this.tableau5.push(new Card(CardSuit.DIAMONDS, CardNumber.Four));
-
-    this.tableau6.push(new Card(CardSuit.CLUBS, CardNumber.Five));
-    this.tableau6.push(new Card(CardSuit.SPADES, CardNumber.Five));
-    this.tableau6.push(new Card(CardSuit.HEARTS, CardNumber.Five));
-    this.tableau6.push(new Card(CardSuit.DIAMONDS, CardNumber.Five));
-    this.tableau6.push(new Card(CardSuit.DIAMONDS, CardNumber.Six));
-
-    this.tableau7.push(new Card(CardSuit.SPADES, CardNumber.Six));
-    this.tableau7.push(new Card(CardSuit.CLUBS, CardNumber.Six));
-    this.tableau7.push(new Card(CardSuit.HEARTS, CardNumber.Six));
-    this.tableau7.push(new Card(CardSuit.DIAMONDS, CardNumber.Seven));
-    this.tableau7.push(new Card(CardSuit.HEARTS, CardNumber.Seven));
-    this.tableau7.push(new Card(CardSuit.SPADES, CardNumber.Seven));
-
-  }
-  
 }
