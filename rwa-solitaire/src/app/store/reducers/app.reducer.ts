a4430e4251dd49c8d2ffb5e64b0d5d879d630622
Milan Stojanovski <milanstojanovski1@gmail.com>
2025-06-02 03:10:25 +0200

    [angular] cleanup / move todo to separate untracked file

--- rwa-solitaire/src/app/store/reducers/app.reducer.ts
+++ rwa-solitaire/src/app/store/reducers/app.reducer.ts
@@ -11,13 +11,3 @@ export const rootReducer: ActionReducerMap<AppState> = {
     userState: userReducer,
     leaderboardsState: leaderboardsReducer,
 };
-
-// redosled
-// 1. pokrenuti nest da vidimo da li sve radi
-// | 1.1 pogledati ono za circular dependency solutions itd, mozda ne mora forward ref itd ako se koriste provideri a ne imports array
-// 2. generisati mock fake podatke za db
-// 3. effects layer za podatke
-// 4. dodati mini animacije
-// 5. dokumentacija / git readme
-// 6. mozda provaliti mail stuff za registraciju
-// 7. naci neki hosting i we go public
